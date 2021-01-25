04d9b54952914f2cdb30a1f15e09088eaba4ca15
Shunpoco <tkngsnsk313320@gmail.com>
2021-01-25 19:45:52 +0900

    write code

---
+++ src/components/MyDate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="date"
+      v-bind="date"
+      v-on:input="date = strToDate($event.target.value)"  
+    />
+    <p>{{ date }}</p>
+    <p>A type of the value is: {{ function () { return typeof date;}() }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+class MyDate extends Vue {
+  @Prop() private date!: Date;
+
+  private strToDate(str: string): Date {
+    return new Date(str);
+  }
+}
+
+export default MyDate;
+</script>
